c5686a434784c124b57ffe815a54ce7c46637907
AliciaMol <54749534+AliciaMol@users.noreply.github.com>
2023-02-27 18:23:15 -0300

    Experiencia y Habilidades
    Se agrega experiencia laboral mas sección de habilidades, funciona 1 de las tres puestas de ejemplo. Hay que seguir revisando.

--- js/funcion.js
+++ js/funcion.js
@@ -1,98 +1,4 @@
-// $.ajax({
-//     url: 'https://randomuser.me/api/',
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//     }
-//   });
-// var contenido = document.querySelector('#contenido')
-// function traer() {
-//     fetch('https://randomuser.me/api/')
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log("hola pues")
-//             console.log(data.results['0'])
-//             contenido.innerHTML = `
-//                 <img src="${data.results['0'].picture.large}" width="100px" class="img-fluid rounded-circle"> 
-//                 <p>Nombre: ${data.results['0'].name.last}</p>
-//                 `
-//         })
-// }
-
-// const url = ' https://randomuser.me/api/?results=1 ';
-
-// let myphoto = document.getElementById('myphoto');
-// let fullname = document.getElementById('fullname');
-// let username = document.getElementById('username');
-// let email = document.getElementById('email');
-// let city = document.getElementById('city');
-// // let btn = document.getElementById('btn');
-
-// // btn.addEventListener("click", function() {
-// //   fetch(url)
-// //     .then(handleErrors)
-// //     .then(parseJSON)
-// //     .then(updateProfile)
-// //     .catch(printError)
-// // });
-
-// function handleErrors(res) {
-//     if (!res.ok) {
-//         throw error(res.status);
-//     }
-//     console.log(res);
-//     return res;
-// }
-
-// function parseJSON(res) {
-//     return res.json();
-// }
-
-// function updateProfile(profile) {
-//     myphoto.src = profile.results[0].picture.medium;
-//     fullname.innerHTML = profile.results[0].name.first + " " + profile.results[0].name.last;
-//     username.innerHTML = profile.results[0].login.username;
-//     email.innerHTML = profile.results[0].email;
-//     city.innerHTML = profile.results[0].location.city;
-//     return 1;
-// }
-
-// function printError(error) {
-//     console.log(error);
-// }
-
-// function createNode(element) {
-//     return document.createElement(element);
-// }
-
-// function append(parent, el) {
-//     return parent.appendChild(el);
-// }
-
-// const person = document.getElementById('person');
-// const url = 'https://randomuser.me/api/?results=1';
-
-// fetch(url)
-//     .then((resp) => resp.json())
-//     .then(function (data) {
-//         let person = data.results;
-//         return person.map(function (persona) {
-//             let li = createNode('li');
-//             let img = createNode('img');
-//             let span = createNode('span');
-//             img.src = author.picture.medium;
-//             span.innerHTML = `${persona.name.first} ${persona.name.last}`;
-//             append(li, img);
-//             append(li, span);
-//             append(ul, li);
-//             console.log(person)
-//         })
-
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
-
+/** --------------- Toma Datos de la API para cargar la info de Persona ---------------*/
 // var contenido = document.querySelector('#contenido')
 var myname = document.getElementById('myname')
 var myphoto = document.getElementById('myphoto')
@@ -107,16 +13,30 @@ fetch(url)                              /* Realizamos una solicitud a esta url*/
     .then(data => {
         // console.log("hola pues")
         console.log(data.results['0'])   /**lo mostramos por consola */
-        
+
         let img_api = data.results['0'].picture.large
-        myphoto.setAttribute("src",img_api);
+        myphoto.setAttribute("src", img_api);
 
         myname.innerHTML = `${data.results['0'].name.first} ${data.results['0'].name.last}`
-
-        email.innerHTML = `${data.results['0'].email}`
-
-        cell.innerHTML = `Cel: ${data.results['0'].cell}`
-        
+        // email.innerHTML = `${data.results['0'].email}`
+        // cell.innerHTML = `Cel: ${data.results['0'].cell}`
     })
     .catch(error => console.log(error))
 
+
+/** ---------------Circular progress---------------*/
+let circularProgress = document.querySelector(".circular-progress"),
+    progressValue = document.querySelector(".progress-value");
+let progressStartValue = 0,
+    progressEndValue = 45,
+    speed = 100;
+
+let progress = setInterval(() => {
+    progressStartValue++;
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style.background = `conic-gradient(#ea3d63 ${progressStartValue * 3.6}deg, #fdeef1 0deg)`
+    // console.log(progressStartValue);
+    if (progressStartValue == progressEndValue) {
+        clearInterval(progress);
+    }
+}, speed);
